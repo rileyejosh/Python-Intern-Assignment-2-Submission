0861670a94089d9a867cdbb00ffed8c2cb5db933
rjiley <39667368+rjiley@users.noreply.github.com>
2018-05-27 04:45:37 -0500

    Add files via upload

---
+++ user.py
@@ -0,0 +1,9 @@
+'''
+Name: Joshua Riley
+Python Intern Assignment 2
+
+'''
+
+for user in TABLE
+    intercom.users.submit(create_items=[{'user_id': user.id, 'name': user.name, 'email': user.email, }])
+
